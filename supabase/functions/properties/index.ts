4fb115927cb51a6516864a650f366efcf47bf881
Frank-NikoAI <frank@4property.com>
2025-10-08 00:56:32 +0100

    Updated index.ts

--- supabase/functions/properties/index.ts
+++ supabase/functions/properties/index.ts
@@ -4,7 +4,7 @@ import { createClient } from 'npm:@supabase/supabase-js@2';
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Client-Info, Apikey, apikey, x-api-token",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Client-Info, Apikey, apikey, x-api-token, token",
 };
 
 Deno.serve(async (req: Request) => {
